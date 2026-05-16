55b354cec50bd74094927f6acbb1c80f3f237423
Vsevolod <VKrupenin55@mail.ru>
2026-05-16 18:20:13 +0300

    DynmicArray get set resize

--- DynamicArray.hpp
+++ DynamicArray.hpp
@@ -52,4 +52,77 @@ public:
     {
         delete[] data;
     }
+
+
+    DynamicArray &operator=(const DynamicArray &other)
+    {
+        if (this != &other)
+        {
+            delete[] data;
+            size = other.size;
+            if (size == 0)
+            {
+                data = nullptr;
+            }
+            else
+            {
+                data = new T[size];
+                std::copy(other.data, other.data + size, data);
+            }
+        }
+        return *this;
+    }
+
+    T Get(size_t index) const
+    {
+        return data[index];
+    }
+
+    void Set(size_t index, const T &value)
+    {
+        data[index] = value;
+    }
+
+    size_t GetSize() const
+    {
+        return size;
+    }
+
+    void Resize(size_t newSize)
+    {
+        if (newSize == size)
+            return;
+
+        T *newData = nullptr;
+        if (newSize > 0)
+        {
+            newData = new T[newSize];
+        }
+
+        size_t copy_count = (newSize < size) ? newSize : size;
+        if (copy_count > 0 && data != nullptr)
+        {
+            std::copy(data, data + copy_count, newData);
+        }
+
+        delete[] data;
+        data = newData;
+        size = newSize;
+    }
+
+    T &operator[](size_t index)
+    {
+        return data[index];
+    }
+
+    const T &operator[](size_t index) const
+    {
+        return data[index];
+    }
+
+    T *begin() { return data; }
+    T *end() { return data + size; }
+
+    const T *begin() const { return data; }
+    const T *end() const { return data + size; }
 };
